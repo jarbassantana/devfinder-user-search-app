cd86c798a0ede20b66a995a120b9bb319088d367
Jarbas Santana <jarbasfilho29@gmail.com>
2021-12-28 15:35:23 -0300

    fix lightmode

--- src/assets/modules/light-mode.js
+++ src/assets/modules/light-mode.js
@@ -1,19 +1,20 @@
+import { sunIcon, moonIcon } from './icons.js'
+
 export default function initLightMode() {
     const btnMode = document.getElementById('userBtnMode')
-    const nameMode = document.getElementById('nameMode')
-
-    function handleClick(event) {
-        const body = document.body
-        body.classList.toggle('darkMode')
-        if (nameMode.innerHTML === 'DARK') {
-            document.getElementById('iconSVG').src = '/icon-sun.a1aadbed.svg'
-            nameMode.innerHTML = 'LIGHT'
-        } else if (nameMode.innerHTML === 'LIGHT') {
-            document.getElementById('iconSVG').src = '/icon-moon.a96a287b.svg'
-            nameMode.innerHTML = 'DARK'
-        }
-    }
-    console.log()
 
     btnMode.addEventListener('click', handleClick)
+}
+
+function handleClick() {
+    const iconContainer = document.getElementById('icon-container')
+    const nameMode = document.getElementById('nameMode')
+
+    if (nameMode.textContent === 'DARK') {
+        nameMode.textContent = 'LIGHT'
+        iconContainer.innerHTML = sunIcon
+        return
+    }
+    nameMode.textContent = 'DARK'
+    iconContainer.innerHTML = moonIcon
 }
