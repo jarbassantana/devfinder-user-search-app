34b503aa1cd5be07f5bb8a37ae5fb0f0d88e869b
Jarbas Santana <jarbasfilho29@gmail.com>
2021-12-28 16:11:16 -0300

    add darkmode in localstorage

--- src/assets/modules/light-mode.js
+++ src/assets/modules/light-mode.js
@@ -4,19 +4,37 @@ export default function initLightMode() {
     const btnMode = document.getElementById('userBtnMode')
 
     btnMode.addEventListener('click', handleClick)
+    window.addEventListener('load', handlePageLoad)
 }
 
 function handleClick() {
     const iconContainer = document.getElementById('icon-container')
     const nameMode = document.getElementById('nameMode')
     const bodyMode = document.querySelector('body')
+
     bodyMode.classList.toggle('darkMode')
     if (nameMode.textContent === 'DARK') {
         nameMode.textContent = 'LIGHT'
         iconContainer.innerHTML = sunIcon
+        localStorage.setItem('colorTheme', 'dark')
         return
     }
 
     nameMode.textContent = 'DARK'
     iconContainer.innerHTML = moonIcon
+    localStorage.setItem('colorTheme', 'light')
+}
+
+function handlePageLoad() {
+    const storedValue = localStorage.getItem('colorTheme')
+    const bodyMode = document.querySelector('body')
+    const iconContainer = document.getElementById('icon-container')
+    const nameMode = document.getElementById('nameMode')
+
+    if (storedValue === 'dark') {
+        bodyMode.classList.add('darkMode')
+        nameMode.textContent = 'LIGHT'
+        iconContainer.innerHTML = sunIcon
+    }
+    console.log(storedValue)
 }
